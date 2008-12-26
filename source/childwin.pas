Dolass/Heidisql

commit faa48e9bb8845cb5c49f134e8ca21c132aa6f1bf

Apply grid changes by click in grid whitespace, rfe #839
git-svn-id: http://heidisql.googlecode.com/svn/trunk@2130 560a3889-bb33-0410-9f33-43a8a102dc09

source/childwin.pas
@@ -457,6 +457,8 @@ type
       NewHeight: Integer; var Resize: Boolean);
     procedure pnlQueryMemoCanResize(Sender: TObject; var NewWidth,
       NewHeight: Integer; var Resize: Boolean);
+    procedure DataGridMouseUp(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
 
     private
       uptime                     : Integer;
@@ -6676,6 +6678,20 @@ procedure TMDIChild.pnlQueryMemoCanResize(Sender: TObject; var NewWidth,
 begin
   // Ensure visibility of query memo while resizing
   Resize := NewWidth >= pnlQueryHelpers.Width + spltQueryHelpers.Width + 40;
+end;
+
+
+procedure TMDIChild.DataGridMouseUp(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+var
+  Grid: TVirtualStringTree;
+  Hit: THitInfo;
+begin
+  // Detect mouse hit in grid whitespace and apply changes.
+  Grid := Sender as TVirtualStringTree;
+  Grid.GetHitTestInfoAt(X, Y, False, Hit);
+  if (Hit.HitNode = nil) or (Hit.HitColumn = NoColumn) or (Hit.HitColumn = InvalidColumn) then
+    DataGridPostUpdateOrInsert(Grid.FocusedNode);
 end;
 
 

source/childwin.dfm
@@ -975,6 +975,7 @@ object MDIChild: TMDIChild
           OnHeaderClick = DataGridHeaderClick
           OnKeyDown = GridKeyDown
           OnKeyUp = controlsKeyUp
+          OnMouseUp = DataGridMouseUp
           OnNewText = DataGridNewText
           Columns = <>
         end

commit 053bc8e605d23ae9c0c74433b834b831a34ba15f

Fix AV, broken in r2130

source/childwin.pas
@@ -6689,6 +6689,8 @@ var
 begin
   // Detect mouse hit in grid whitespace and apply changes.
   Grid := Sender as TVirtualStringTree;
+  if not Assigned(Grid.FocusedNode) then
+    Exit;
   Grid.GetHitTestInfoAt(X, Y, False, Hit);
   if (Hit.HitNode = nil) or (Hit.HitColumn = NoColumn) or (Hit.HitColumn = InvalidColumn) then
     DataGridPostUpdateOrInsert(Grid.FocusedNode);