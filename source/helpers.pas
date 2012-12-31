Dolass/Heidisql

commit c55d8aee109b441f59c584a8687763f88a46b5db

Block close button on query tab while its query thread is running. Fixes issue #3063.
git-svn-id: http://heidisql.googlecode.com/svn/trunk@4283 560a3889-bb33-0410-9f33-43a8a102dc09

source/main.pas
@@ -9576,9 +9576,12 @@ var
   Tab: TQueryTab;
 begin
   Tab := QueryTabs[PageIndex-tabQuery.PageIndex];
-  if (not Tab.Memo.Modified) or (not AppSettings.ReadBool(asPromptSaveFileOnTabClose)) then
-    Result := True
-  else begin
+  if Tab.QueryRunning then begin
+    LogSQL(_('Cannot close tab with running query. Please wait until query has finished.'));
+    Result := False;
+  end else if not Tab.Memo.Modified then begin
+    Result := True;
+  end else begin
     // Unhide tabsheet so the user sees the memo content
     Tab.TabSheet.PageControl.ActivePage := Tab.TabSheet;
     if Tab.MemoFilename <> '' then

out/locale/en/LC_MESSAGES/default.po
@@ -7,7 +7,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: HeidiSQL\n"
 "POT-Creation-Date: 2012-11-05 21:40\n"
-"PO-Revision-Date: 2012-12-13 21:33+0100\n"
+"PO-Revision-Date: 2012-12-18 19:28+0100\n"
 "Last-Translator: Ansgar Becker <heidisql@anse.de>\n"
 "Language-Team: English (http://www.transifex.com/projects/p/heidisql/"
 "language/en/)\n"
@@ -5562,3 +5562,9 @@ msgstr "Minute"
 #. Table tools
 msgid "Second"
 msgstr "Second"
+
+#. Query tab close button
+msgid ""
+"Cannot close tab with running query. Please wait until query has finished."
+msgstr ""
+"Cannot close tab with running query. Please wait until query has finished."

commit 1fef9ecbdb6c8112f0b44f61f87d8de41c525aab

Introduce "KeepAskingSetting" parameter to MessageDialog(), which activates a verification checkbox left besides the dialog buttons. Use that when confirming to close a query tab, so the user can disable the dialog within the dialog itself, without the need to disable this setting in Preferences > SQL. Fixes issue #3065, which was broken since r4283.

source/helpers.pas
@@ -172,7 +172,7 @@ type
     asFieldEditorSet, asFieldNullBackground, asGroupTreeObjects, asDisplayObjectSizeColumn, asSQLfile,
     asActionShortcut1, asActionShortcut2, asHighlighterForeground, asHighlighterBackground, asHighlighterStyle,
     asListColWidths, asListColsVisible, asListColPositions, asListColSort, asSessionFolder,
-    asRecentFilter, asDateTimeEditorCursorPos, asAppLanguage, asAutoExpand);
+    asRecentFilter, asDateTimeEditorCursorPos, asAppLanguage, asAutoExpand, asUnused);
   TAppSetting = record
     Name: String;
     Session: Boolean;
@@ -316,7 +316,7 @@ type
   function IsEmpty(Str: String): Boolean;
   function IsNotEmpty(Str: String): Boolean;
   function MessageDialog(const Msg: string; DlgType: TMsgDlgType; Buttons: TMsgDlgButtons): Integer; overload;
-  function MessageDialog(const Title, Msg: string; DlgType: TMsgDlgType; Buttons: TMsgDlgButtons): Integer; overload;
+  function MessageDialog(const Title, Msg: string; DlgType: TMsgDlgType; Buttons: TMsgDlgButtons; KeepAskingSetting: TAppSettingIndex=asUnused): Integer; overload;
   function ErrorDialog(Msg: string): Integer; overload;
   function ErrorDialog(const Title, Msg: string): Integer; overload;
   function GetHTMLCharsetByEncoding(Encoding: TEncoding): String;
@@ -2397,13 +2397,14 @@ begin
 end;
 
 
-function MessageDialog(const Title, Msg: string; DlgType: TMsgDlgType; Buttons: TMsgDlgButtons): Integer;
+function MessageDialog(const Title, Msg: string; DlgType: TMsgDlgType; Buttons: TMsgDlgButtons; KeepAskingSetting: TAppSettingIndex=asUnused): Integer;
 var
   m: String;
   Dialog: TTaskDialog;
   Btn: TTaskDialogButtonItem;
   MsgButton: TMsgDlgBtn;
   rx: TRegExpr;
+  KeepAskingValue: Boolean;
 
   procedure AddButton(BtnCaption: String; BtnResult: TModalResult);
   begin
@@ -2413,7 +2414,13 @@ var
   end;
 begin
   if (Win32MajorVersion >= 6) and ThemeServices.ThemesEnabled then begin
+    // Use modern task dialog on Vista and above
     Dialog := TTaskDialog.Create(nil);
+    Dialog.Flags := [tfEnableHyperlinks, tfAllowDialogCancellation];
+    Dialog.CommonButtons := [];
+    Dialog.OnHyperlinkClicked := MainForm.TaskDialogHyperLinkClicked;
+
+    // Caption, title and text
     case DlgType of
       mtWarning: Dialog.Caption := _('Warning');
       mtError: Dialog.Caption := _('Error');
@@ -2426,9 +2433,8 @@ begin
     rx.Expression := 'https?\:\/\/\S+';
     Dialog.Text := rx.Replace(Msg, '<a href="$0">$0</a>', True);
     rx.Free;
-    Dialog.Flags := [tfEnableHyperlinks, tfAllowDialogCancellation];
-    Dialog.CommonButtons := [];
-    Dialog.OnHyperlinkClicked := MainForm.TaskDialogHyperLinkClicked;
+
+    // Main icon, and footer link
     case DlgType of
       mtWarning:      Dialog.MainIcon := tdiWarning;
       mtError: begin
@@ -2440,6 +2446,8 @@ begin
       mtConfirmation: Dialog.MainIcon := tdiInformation;
       else            Dialog.MainIcon := tdiNone;
     end;
+
+    // Add buttons
     for MsgButton in Buttons do begin
       case MsgButton of
         mbYes:       AddButton('Yes', mrYes);
@@ -2455,10 +2463,29 @@ begin
         mbClose:     AddButton('Close', mrClose);
       end;
     end;
-    Dialog.Execute;
-    Result := Dialog.ModalResult;
+
+    // Checkbox, s'il vous plait?
+    KeepAskingValue := True;
+    if KeepAskingSetting <> asUnused then begin
+      if not (mbNo in Buttons) then
+        raise Exception.CreateFmt(_('Missing "No" button in %() call'), ['MessageDialog']);
+      KeepAskingValue := AppSettings.ReadBool(KeepAskingSetting);
+      Dialog.Flags := Dialog.Flags + [tfVerificationFlagChecked];
+      Dialog.VerificationText := _('Keep asking this question.');
+    end;
+
+    // Supress dialog and assume "No" if user disabled this dialog
+    if KeepAskingValue then begin
+      Dialog.Execute;
+      Result := Dialog.ModalResult;
+      if (KeepAskingSetting <> asUnused) and (not (tfVerificationFlagChecked in Dialog.Flags)) then
+        AppSettings.WriteBool(KeepAskingSetting, False);
+    end else
+      Result := mrNo;
+
     Dialog.Free;
   end else begin
+    // Backwards compatible dialog on Windows XP
     m := Msg;
     if Title <> '' then
       m := Title + CRLF + CRLF + m;

source/main.pas
@@ -9603,7 +9603,7 @@ begin
       msg := f_('Save changes to file %s ?', [Tab.MemoFilename])
     else
       msg := f_('Save content of tab "%s"?', [Trim(Tab.TabSheet.Caption)]);
-    case MessageDialog(_('Modified query'), msg, mtConfirmation, [mbYes, mbNo, mbCancel]) of
+    case MessageDialog(_('Modified query'), msg, mtConfirmation, [mbYes, mbNo, mbCancel], asPromptSaveFileOnTabClose) of
       mrNo: Result := True;
       mrYes: begin
         if Tab.MemoFilename <> '' then